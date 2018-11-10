6887343a7c5378eba7f07ef3df356d293b788bcc
sebastian <sebastianh.16592@gmail.com>
2018-11-10 11:54:57 -0500

    Loguin

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/messages/{message}','MessagesController@show');
 
 Route::post('/messages/create','MessagesController@create');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
